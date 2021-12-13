8982692762ffa6392a416f5c92b11b8eeb10809c
raheemadamboev <raheemideadev@gmail.com>
2021-12-13 21:02:13 +0500

    set up event model
    Took 40 minutes

---
+++ app/src/main/java/xyz/teamgravity/aliftech/domain/model/EventModel.kt
@@ -0,0 +1,10 @@
+package xyz.teamgravity.aliftech.domain.model
+
+import java.util.*
+
+data class EventModel(
+    val name: String = "",
+    val iconUrl: String = "",
+    val contentUrl: String = "",
+    val endDate: Date? = null
+)
