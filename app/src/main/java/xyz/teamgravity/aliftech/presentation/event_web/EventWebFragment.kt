aba8aa90347a7200011c89d53c5d101ebe3050ac
raheemadamboev <raheemideadev@gmail.com>
2021-12-13 23:22:16 +0500

    fragments added, binding setup for fragments, layout added for fragments
    Took 6 minutes

---
+++ app/src/main/java/xyz/teamgravity/aliftech/presentation/event_web/EventWebFragment.kt
@@ -0,0 +1,24 @@
+package xyz.teamgravity.aliftech.presentation.event_web
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import xyz.teamgravity.aliftech.databinding.FragmentEventWebBinding
+
+class EventWebFragment : Fragment() {
+
+    private var _binding: FragmentEventWebBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentEventWebBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
