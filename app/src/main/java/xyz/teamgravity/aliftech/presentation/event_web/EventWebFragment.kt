a9d2f1c37a4e2d64a13a04fc54ab9aa47698c3ed
raheemadamboev <raheemideadev@gmail.com>
2021-12-14 03:59:59 +0500

    event web fragment is set up to load url within web view and navigate safely extension function is added
    Took 5 minutes

--- app/src/main/java/xyz/teamgravity/aliftech/presentation/event_web/EventWebFragment.kt
+++ app/src/main/java/xyz/teamgravity/aliftech/presentation/event_web/EventWebFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.teamgravity.aliftech.databinding.FragmentEventWebBinding
 import javax.inject.Inject
@@ -16,12 +17,27 @@ class EventWebFragment : Fragment() {
     private var _binding: FragmentEventWebBinding? = null
     private val binding get() = _binding!!
 
+    private val args by navArgs<EventWebFragmentArgs>()
+
     @Inject
     lateinit var client: WebViewClient
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentEventWebBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        webview()
+    }
+
+    private fun webview() {
+        binding.webview.apply {
+            webViewClient = client
+            loadUrl(args.url)
+        }
     }
 
     override fun onDestroyView() {
