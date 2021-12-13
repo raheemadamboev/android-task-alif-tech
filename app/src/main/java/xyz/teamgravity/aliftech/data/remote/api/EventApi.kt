d58c8f6cbe0b9630e07ede31de3b1a09b83ac008
raheemadamboev <raheemideadev@gmail.com>
2021-12-13 21:05:58 +0500

    base url added to event api, resource wrapper class added to core package
    Took 4 minutes

--- app/src/main/java/xyz/teamgravity/aliftech/data/remote/api/EventApi.kt
+++ app/src/main/java/xyz/teamgravity/aliftech/data/remote/api/EventApi.kt
@@ -5,6 +5,10 @@ import xyz.teamgravity.aliftech.data.remote.dto.EventResponseDto
 
 interface EventApi {
 
+    companion object {
+        const val BASE_URL = "https://guidebook.com/"
+    }
+
     @GET(value = "/service/v2/upcomingGuides")
     suspend fun getEvents(): EventResponseDto
 }
